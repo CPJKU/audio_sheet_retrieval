dc5b66d78953a66a0bd1fc5f89e293bf27b0643e
Stefan Balke <stefan.balke@jku.at>
2019-03-19 16:45:45 +0100

    update script

--- audio_sheet_retrieval/train_models_ISMIR19.sh
+++ audio_sheet_retrieval/train_models_ISMIR19.sh
@@ -1,26 +1,26 @@
-#!/bin/bash
+#! /bin/sh
+function run_experiment {
+  device="export THEANO_FLAGS=\"device=$1\""
 
-# example calls:
-# --------------
-# ./train_models_ISMIR19.sh cuda0 models/mutopia_ccal_cont.py ../../sheet_manager/sheet_manager/splits/all_split.yaml
+  cmd="python run_train.py --model $2 --data mutopia --train_split $3 --config exp_configs/$4.yaml"
+  echo $device
+  echo $cmd
+  ($device && $cmd)
 
-# $1 ... device
-# $2 ... model
-# $3 ... train split
+  cmd="python refine_cca.py --model $2 --data mutopia --train_split $3 --config exp_configs/$4.yaml --n_train 25000"
+  echo $device
+  echo $cmd
+  ($device && $cmd)
+}
 
-model_names=mutopia_ccal_cont_rsz_gap mutopia_ccal_cont_rsz_dense mutopia_ccal_cont_rsz_dense_att
+# BL1
+run_experiment cuda0 models/mutopia_ccal_cont_rsz_gap ../../msmd/msmd/splits/all_split.yaml mutopia_full_aug_sc
 
-for c in mutopia_full_aug_sc, mutopia_full_aug_mc, mutopia_full_aug_lc
-do
-    device="export THEANO_FLAGS=\"device=$1\""
+# BL2
+run_experiment cuda0 models/mutopia_ccal_cont_rsz_dense ../../msmd/msmd/splits/all_split.yaml mutopia_full_aug_sc
+run_experiment cuda0 models/mutopia_ccal_cont_rsz_dense ../../msmd/msmd/splits/all_split.yaml mutopia_full_aug_mc
+run_experiment cuda0 models/mutopia_ccal_cont_rsz_dense ../../msmd/msmd/splits/all_split.yaml mutopia_full_aug_lc
 
-    cmd="python run_train.py --model $2 --data mutopia --train_split $3 --config exp_configs/$c.yaml"
-    echo $device
-    echo $cmd
-    ($device && $cmd)
-
-    cmd="python refine_cca.py --model $2 --data mutopia --train_split $3 --config exp_configs/$c.yaml --n_train 25000"
-    echo $device
-    echo $cmd
-    ($device && $cmd)
-done
+# BL2 + Att
+run_experiment cuda0 models/mutopia_ccal_cont_rsz_dense_att ../../msmd/msmd/splits/all_split.yaml mutopia_full_aug_mc
+run_experiment cuda0 models/mutopia_ccal_cont_rsz_dense_att ../../msmd/msmd/splits/all_split.yaml mutopia_full_aug_lc
