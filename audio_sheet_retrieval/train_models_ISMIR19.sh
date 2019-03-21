523bc252ba69d10cc1b69b49b7b2ee1a7bcf33be
Stefan Balke <stefan@balke.at>
2019-03-21 09:28:09 +0100

    refined train.sh script

--- audio_sheet_retrieval/train_models_ISMIR19.sh
+++ audio_sheet_retrieval/train_models_ISMIR19.sh
@@ -1,26 +1,27 @@
 #! /bin/sh
-function run_experiment {
-  device="export THEANO_FLAGS=\"device=$1\""
-
-  cmd="python run_train.py --model $2 --data mutopia --train_split $3 --config exp_configs/$4.yaml"
-  echo $device
+run_experiment () {
+  cmd="python run_train.py --model $1 --data mutopia --train_split $2 --config exp_configs/$3.yaml"
   echo $cmd
-  ($device && $cmd)
+  $cmd
 
-  cmd="python refine_cca.py --model $2 --data mutopia --train_split $3 --config exp_configs/$4.yaml --n_train 25000"
-  echo $device
+  cmd="python refine_cca.py --model $1 --data mutopia --train_split $2 --config exp_configs/$3.yaml --n_train 25000"
   echo $cmd
-  ($device && $cmd)
+  $cmd
 }
 
+
+device="export THEANO_FLAGS='device=${1}'"
+echo $device
+$device
+
 # BL1
-run_experiment cuda0 models/mutopia_ccal_cont_rsz_gap ../../msmd/msmd/splits/all_split.yaml mutopia_full_aug_sc
+run_experiment models/mutopia_ccal_cont_rsz_gap ../../msmd/msmd/splits/all_split.yaml mutopia_full_aug_sc
 
 # BL2
-run_experiment cuda0 models/mutopia_ccal_cont_rsz_dense ../../msmd/msmd/splits/all_split.yaml mutopia_full_aug_sc
-run_experiment cuda0 models/mutopia_ccal_cont_rsz_dense ../../msmd/msmd/splits/all_split.yaml mutopia_full_aug_mc
-run_experiment cuda0 models/mutopia_ccal_cont_rsz_dense ../../msmd/msmd/splits/all_split.yaml mutopia_full_aug_lc
+run_experiment models/mutopia_ccal_cont_rsz_dense ../../msmd/msmd/splits/all_split.yaml mutopia_full_aug_sc
+run_experiment models/mutopia_ccal_cont_rsz_dense ../../msmd/msmd/splits/all_split.yaml mutopia_full_aug_mc
+run_experiment models/mutopia_ccal_cont_rsz_dense ../../msmd/msmd/splits/all_split.yaml mutopia_full_aug_lc
 
 # BL2 + Att
-run_experiment cuda0 models/mutopia_ccal_cont_rsz_dense_att ../../msmd/msmd/splits/all_split.yaml mutopia_full_aug_mc
-run_experiment cuda0 models/mutopia_ccal_cont_rsz_dense_att ../../msmd/msmd/splits/all_split.yaml mutopia_full_aug_lc
+run_experiment models/mutopia_ccal_cont_rsz_dense_att ../../msmd/msmd/splits/all_split.yaml mutopia_full_aug_mc
+run_experiment models/mutopia_ccal_cont_rsz_dense_att ../../msmd/msmd/splits/all_split.yaml mutopia_full_aug_lc
