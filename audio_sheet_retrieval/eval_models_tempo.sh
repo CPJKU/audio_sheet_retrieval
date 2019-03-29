672a091154b0272b2205fe6b392c27660d961128
Stefan Balke <stefan.balke@jku.at>
2019-03-29 16:23:03 +0100

    model tempo evaluation for --estimate_UV as well

--- audio_sheet_retrieval/eval_models_tempo.sh
+++ audio_sheet_retrieval/eval_models_tempo.sh
@@ -9,17 +9,22 @@
 # $3 ... train split
 # $4 ... config file
 
+device="export THEANO_FLAGS=\"device=$1\""
+
 for t in 0.5 0.75 1.0 1.25 1.5 1.75 2.0
 do
-    device="export THEANO_FLAGS=\"device=$1\""
+    for est_UV in "" "--estimate_UV"
+    do
+        # sheet to audio
+        cmd="python run_eval.py --model $2 --data mutopia --train_split $3 --config $4 --dump_results --test_tempo $t --n_test 10000 $est_UV"
+        echo $device
+        echo $cmd
+        ($device && $cmd)
 
-    cmd="python run_eval.py --model $2 --data mutopia --train_split $3 --config $4 --estimate_UV --dump_results --test_tempo $t --n_test 10000"
-    echo $device
-    echo $cmd
-    ($device && $cmd)
-
-    cmd="$cmd --V2_to_V1"
-    echo $device
-    echo $cmd
-    ($device && $cmd)
+        # audio to sheet
+        cmd="$cmd --V2_to_V1"
+        echo $device
+        echo $cmd
+        ($device && $cmd)
+    done
 done
